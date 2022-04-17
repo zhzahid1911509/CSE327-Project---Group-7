e560d8c91867c05abfaef3bc3903514db8cf0cd7
Rafidul Islam <68073987+rafidulislam19@users.noreply.github.com>
2022-04-18 01:36:41 +0600

    Update web.php
    web.php is updated

--- Virtual Health Service BD/routes/web.php
+++ Virtual Health Service BD/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController; //mentioning HomeController
+
+use App\Http\Controllers\AdminController; //mentioning AdminController
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',[HomeController::class,'index']);
+
+Route::get('/home',[HomeController::class,'redirect']); //creating another route
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/add_doctor_view',[AdminController::class,'addview']);
 });
