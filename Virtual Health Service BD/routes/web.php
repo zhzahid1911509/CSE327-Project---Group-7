3d396dd2140eff595daa572bce11e8715844cc71
Towsif Muhtadi Khan <67815556+TowsifMuhtadiKhan@users.noreply.github.com>
2022-04-19 01:28:10 +0600

    Updated web.php added the necessary route
    added the necessary route

--- Virtual Health Service BD/routes/web.php
+++ Virtual Health Service BD/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/', [HomeController::class, 'index'] );
+
+Route::get('/home', [HomeController::class, 'redirect'] )->middleware('auth','verified');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
+
+Route::get('/add_doctor_view', [AdminController::class, 'addview'] );
+
+Route::post('/upload_doctor', [AdminController::class, 'upload'] );
+
+Route::post('/appointment', [HomeController::class, 'appointment'] );
+
+Route::get('/myappointment', [HomeController::class, 'myappointment'] );
+
+Route::get('/cancel_appoint/{id}', [HomeController::class, 'cancel_appoint'] );
+
+Route::get('/showappointment', [AdminController::class, 'showappointment'] );
+
+Route::get('/approved/{id}', [AdminController::class, 'approved'] );
+
+Route::get('/canceled/{id}', [AdminController::class, 'canceled'] );
+
+Route::get('/showdoctor', [AdminController::class, 'showdoctor'] );
+
+Route::get('/deletedoctor/{id}', [AdminController::class, 'deletedoctor'] );
+
+Route::get('/updatedoctor/{id}', [AdminController::class, 'updatedoctor'] );
+
+Route::post('/editdoctor/{id}', [AdminController::class, 'editdoctor'] );
+
+Route::get('/emailview/{id}', [AdminController::class, 'emailview'] );
+
+
+Route::post('/sendemail/{id}', [AdminController::class, 'sendemail'] );
