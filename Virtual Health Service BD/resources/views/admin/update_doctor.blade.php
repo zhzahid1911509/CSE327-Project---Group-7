29d4ed8e3a7b6a56a9a511f1883c7d24597cd3ea
Towsif Muhtadi Khan <67815556+TowsifMuhtadiKhan@users.noreply.github.com>
2022-04-19 01:00:46 +0600

    Update doctor part is added and modified
    Added and modified update doctor

--- Virtual Health Service BD/resources/views/admin/update_doctor.blade.php
+++ Virtual Health Service BD/resources/views/admin/update_doctor.blade.php
@@ -42,7 +42,65 @@
         <div class="container-fluid page-body-wrapper">
 
         
-        
+           <div class="container" align="center" style="padding:100px">
+
+            @if(session()->has('message'))
+
+                <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert">
+                        x
+                    </button>
+
+                    {{session()->get('message')}}
+
+                </div>
+
+                @endif
+
+           <form action="{{url('editdoctor',$data->id)}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+          
+           <div style="padding:15px">
+                <label> Doctor Name</label>
+                <input type="text" style="color:black" name="name" value="{{$data->name}}">
+            </div>
+
+            <div style="padding:15px">
+                <label> Phone</label>
+                <input type="number" style="color:black" name="phone" value="{{$data->phone}}">
+            </div>
+
+            <div style="padding:15px">
+                <label>Speciality</label>
+                <input type="text" style="color:black" name="speciality" value="{{$data->speciality}}">
+            </div>
+
+            <div style="padding:15px">
+                <label> Room</label>
+                <input type="text" style="color:black" name="room" value="{{$data->room}}">
+            </div>
+
+            <div style="padding:15px">
+                <label> Old image</label>
+                <img height="150" width="150"src="doctorimage/{{$data->image}}">
+            </div>
+
+            <div style="padding:15px">
+                <label> Change image</label>
+                <input type="file" name="file">
+            </div>
+
+            <div style="padding:15px">
+                
+                <input type="submit" name="btn btn-success">
+            </div>
+           </form>
+
+          
+
+
+           </div>
+        </div>
    
     <!-- container-scroller -->
     <!-- plugins:js -->
