75925589000f2ed212f9a0a185ea2dd428ccc314
Towsif Muhtadi Khan <67815556+TowsifMuhtadiKhan@users.noreply.github.com>
2022-04-19 00:22:27 +0600

    Update AdminController.php
    Added the doctor information update update

--- Virtual Health Service BD/app/Http/Controllers/AdminController.php
+++ Virtual Health Service BD/app/Http/Controllers/AdminController.php
@@ -65,3 +65,50 @@ create variables for taking user inputs and storing
       $data->save();
       return redirect()->back();
    }
+    public function showdoctor()
+   {
+       $data=doctor::all();
+       return view('admin.showdoctor',compact('data'));
+   }
+   public function deletedoctor ($id)
+   {
+      $data=doctor::find($id);
+
+      $data->delete();
+
+      return redirect()->back();
+   }
+   public function updatedoctor($id)
+   {
+      $data=doctor::find($id);
+      return view('admin.update_doctor', compact('data'));
+   }
+   public function editdoctor(Request $request, $id)
+   {
+      $doctor=doctor::find($id);
+
+      $doctor->name=$request->name;
+
+      $doctor->phone=$request->phone;
+
+      $doctor->speciality=$request->speciality;
+
+      $doctor->room=$request->room;
+
+      $image=$request->file;
+
+      if($image)
+      {
+         $imagename=time().'.'.$image->getClientOriginalExtension();
+
+         $request->file->move('doctorimage',$imagename);
+   
+         $doctor->image=$imagename;
+
+      }
+
+      $doctor->save();
+
+      return redirect()->back()->with('message','Doctor info updated succssfully');
+
+   }
